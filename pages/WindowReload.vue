aac643acfdc3261a1282e33bf3ed907d85f04b2c
Koichi Ezato <koichi.e.38@gmail.com>
2022-02-10 15:27:13 +0900

    window.reload

---
+++ pages/WindowReload.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card>
+    <v-card-title>Window Reload</v-card-title>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'WindowReload',
+  mounted() {
+    this.pageReload()
+  },
+  methods: {
+    pageReload() {
+      if (window.name !== 'reloaded') {
+        alert('Window Reload!!')
+        window.name = 'reloaded'
+        location.reload()
+      } else {
+        window.name = ''
+      }
+    },
+  },
+}
+</script>
