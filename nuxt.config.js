1794a40667802b5e20de8bb63d00d09ca94af104
Koichi Ezato <koichi.e.38@gmail.com>
2022-02-15 17:00:59 +0900

    デフォルトフォント変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,14 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css2?family=Noto+Sans+JP&family=Oswald:wght@700&family=Roboto&display=swap',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -61,6 +68,7 @@ export default {
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
+    treeShake: true,
     theme: {
       dark: true,
       themes: {
---
+++ pages/DefaultFontChange.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-card>
+    <v-card-title>デフォルトフォント変更</v-card-title>
+    <v-card-text>日本語フォントが綺麗になりました。</v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'DefaultFontChange'
+}
+</script>
