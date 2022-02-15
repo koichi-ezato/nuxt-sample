cdf0fc3cf563b76c74f58f6b42db6c781bac8ba1
Koichi Ezato <koichi.e.38@gmail.com>
2022-02-15 18:24:14 +0900

    子コンポーネントのメソッドを実行する

---
+++ components/Child.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-card-text v-text='value' />
+</template>
+
+<script>
+export default {
+  name: 'ChildComponent',
+  data() {
+    return {
+      value: '子コンポーネント',
+    }
+  },
+  methods: {
+    changeValue() {
+      this.value = '子コンポーネントのメソッドが実行されました'
+    }
+  },
+}
+</script>
---
+++ pages/ParentComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card>
+    <v-card-title>親コンポーネント</v-card-title>
+    <child-component ref='child' />
+    <v-card-actions>
+      <v-btn tile color='primary' @click='execChildComponent'>子コンポーネントのメソッド実行</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import ChildComponent from '~/components/Child'
+
+export default {
+  name: 'ParentComponent',
+  components: { ChildComponent },
+  methods: {
+    execChildComponent() {
+      this.$refs.child.changeValue()
+    }
+  }
+}
+</script>
